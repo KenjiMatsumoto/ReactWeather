e4d93caacdbfb15b03cc1ce476fdd1f9eb499d56
Kenji Matsumoto <kenjimatsumoto1983@gmail.com>
2016-05-29 20:44:35 +0900

    add foundation & jquery

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,22 @@
+var webpack = require('webpack');
+
 module.exports = {
   //ビルドの起点となるファイルパスの指定
   //Arrayやobjectでの複数指定が可能
-  entry: './app/app.jsx',
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
   output: {
     //出力先のファイルパスを指定
     path: __dirname,
